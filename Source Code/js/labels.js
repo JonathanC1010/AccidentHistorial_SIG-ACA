c1b433618132e3689d7c5aec055520103c5a1e81
RIADAPLE <54406872+RIADAPLE@users.noreply.github.com>
2023-07-03 15:47:53 -0600

    Update labels.js

--- Source Code/js/labels.js
+++ Source Code/js/labels.js
@@ -1,3 +1,10 @@
+/*
+* Nombre del archivo: Autolinker.min.js
+* Autor: Rodrigo Antonio Ungo Muñoz
+* Licencia: GPL v2.0
+* Año: 2023
+*/
+
 var hideLabel = function(label) {
     label.labelObject.style.opacity = 0;
     label.labelObject.style.transition = 'opacity 0s';
