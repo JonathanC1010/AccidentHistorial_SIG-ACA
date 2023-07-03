69ed847a47dc1884832dbf5b2e6319082064bed1
RIADAPLE <54406872+RIADAPLE@users.noreply.github.com>
2023-07-03 15:50:33 -0600

    Update leaflet.js

--- Source Code/js/leaflet.js
+++ Source Code/js/leaflet.js
@@ -1,3 +1,10 @@
+/*
+* Nombre del archivo: leaflet.js
+* Autor: Rodrigo Antonio Ungo Muñoz
+* Licencia: GPL v2.0
+* Año: 2023
+*/
+
 /* @preserve
  * Leaflet 1.6.0+Detached: 0c81bdf904d864fd12a286e3d1979f47aba17991.0c81bdf, a JS library for interactive maps. http://leafletjs.com
  * (c) 2010-2019 Vladimir Agafonkin, (c) 2010-2011 CloudMade
